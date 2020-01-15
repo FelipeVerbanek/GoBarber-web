dbf3ac77aef51b1a2dfb5454d68f9d4eeb6c8e67
felipeverbanek <felipeverbanek@gmail.com>
2020-01-14 23:19:11 -0300

    first commit

---
+++ src/store/modules/auth/reducer.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {
+    token: 'teste'
+}
+
+export default function auth(state = INITIAL_STATE, action){
+    switch(action.type){
+        default:
+            return state;
+    }
+}
