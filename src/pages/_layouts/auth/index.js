3bf3bd0ac591775e0e630a25308d067e44d3c4e5
felipeverbanek <felipeverbanek@gmail.com>
2020-01-18 13:18:28 -0300

    estilizacao header e notifications

--- src/pages/_layouts/auth/index.js
+++ src/pages/_layouts/auth/index.js
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types'
 
 import { Wrapper, Content } from './styles';
 
+
+
 export default function AuthLayouts({children}) {
   return (
-  <Wrapper><Content>{children}</Content></Wrapper>
+  <Wrapper>
+      <Content>{children}</Content>
+    </Wrapper>
   );
 }
 
